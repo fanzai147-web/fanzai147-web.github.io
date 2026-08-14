9c5ecfe300c0f5c1bd684b452fc48381478dcd64
Felix5 <fanzai147@gmail.com>
2026-08-14 15:37:27 +0800

    Add application route scripts

---
+++ _next/static/chunks/app/layout-ff3cd95bd78ff19b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7465:()=>{},8017:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7465,23))}},e=>{e.O(0,[266,212,424,358],()=>e(e.s=8017)),_N_E=e.O()}]);
